ba2baf86e5396c629de0cbc12d584effdfe7cc04
Sean Craig <seancraig21@gmail.com>
2021-10-06 21:23:04 -0500

    Added timeblock coloring based on what time it is

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,29 @@
 var currentDate = moment().format("MMMM Do YYYY")
 
 $("#currentDay").text(currentDate);
+
+$(document).ready(function () {
+  function time() {
+    var currentTime = moment().hour();
+
+    //debug stuff
+    var debugTime = moment().set('hour', 10).hour();
+    currentTime = debugTime;
+    console.log("current time is "+currentTime);
+
+    // loop through timeblocks
+    $('.time-block').each(function(){
+      var timeBlock = parseInt($(this).attr("id"))
+      // console.log(timeBlock);
+      if (timeBlock < currentTime) {
+        $(this).addClass("past");
+      } else if (timeBlock === currentTime) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  time();
+});
