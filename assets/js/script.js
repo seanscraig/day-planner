78a2b74331283b652e0345e63dca14e9718dc64f
Sean Craig <seancraig21@gmail.com>
2021-10-07 15:13:20 -0500

    Added saving feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,27 @@
 var currentDate = moment().format("MMMM Do YYYY");
 
+// set the current time
 $("#currentDay").text(currentDate);
 
+// make sure the document is loaded before manipulating it
 $(document).ready(function () {
   function time() {
     var currentTime = moment().hour();
 
     //debug stuff
-    var debugTime = moment().set("hour", 10).hour();
-    currentTime = debugTime;
+    // var debugTime = moment().set("hour", 10).hour();
+    // currentTime = debugTime;
     console.log("current time is " + currentTime);
 
     // loop through timeblocks
-    $(".time-block").each(function () {
+    $('.time-block').each(function () {
+      
+      // pull out the int from the timeblock id
       var timeBlock = parseInt($(this).attr("id"));
-      // console.log(timeBlock);
+
+      //check to see if the timeblock is in the past present or future
       if (timeBlock < currentTime) {
         $(this).addClass("past");
-        // $(this).attr("disabled", "disabled");
       } else if (timeBlock === currentTime) {
         $(this).addClass("present");
       } else {
@@ -25,6 +29,27 @@ $(document).ready(function () {
       }
     });
   }
+
+  // when the save button is clicked it 
+  $('.saveBtn').on('click', function(){
+    var text = $(this).siblings('.description').val();
+    var time = $(this).parent().attr('id');
+
+    localStorage.setItem(time, text);
+
+    alert("yay!");
+  });
+
+  console.log(localStorage.getItem('9hour'));
+  $('#9hour .description').val(localStorage.getItem('9hour'));
+  $('#10hour .description').val(localStorage.getItem('10hour'));
+  $('#11hour .description').val(localStorage.getItem('11hour'));
+  $('#12hour .description').val(localStorage.getItem('12hour'));
+  $('#13hour .description').val(localStorage.getItem('13hour'));
+  $('#14hour .description').val(localStorage.getItem('14hour'));
+  $('#15hour .description').val(localStorage.getItem('15hour'));
+  $('#16hour .description').val(localStorage.getItem('16hour'));
+  $('#17hour .description').val(localStorage.getItem('17hour'));
 
   time();
 });
